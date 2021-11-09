f9e1324035623b9826cd203136d5aaadd7a649a0
breakthatbass <gamache.taylor@gmail.com>
2021-11-09 18:45:15 -0500

    clean dir, upload more programs

---
+++ src/blink_both_LEDS.c
@@ -0,0 +1,36 @@
+/**
+ * buttons.c - a simple program to make use of the two buttons
+ *
+ * left button turns on the left LED
+ * right button turns on the right LED
+ *
+ * Taylor Gamache, 2021-11-09
+ * */
+#include <msp430fr5994.h>
+
+#define BLINK_CYCLES 500000L
+
+void main(void)
+{
+	// put the dog to sleep
+	WDTCTL = WDTPW | WDTHOLD;
+	// unlock ports
+	PM5CTL0 &= ~LOCKLPM5;
+
+	// set LED 1 (pin 1.0) as output
+	P1DIR |= BIT0;
+	// set LED 1 off (low)
+	P1OUT &= ~BIT0; 
+
+	// set LED 2 (pin 1.1) as output
+	P1DIR |= BIT1;
+	// set LED 2 off (low)
+	P1OUT &= ~BIT1;
+
+	while (1) {
+		__delay_cycles(BLINK_CYCLES);
+		// if on, turn off, if off, tuen on
+		P1OUT ^= BIT0;
+		P1OUT ^= BIT1;
+	}
+}
