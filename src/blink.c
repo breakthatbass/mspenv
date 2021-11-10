6fcda16556c064a1b33363776ca9f6431eeb4873
breakthatbass <gamache.taylor@gmail.com>
2021-11-10 10:21:06 -0500

    fix comment typos

--- src/blink.c
+++ src/blink.c
@@ -1,10 +1,7 @@
 /**
- * buttons.c - a simple program to make use of the two buttons
+ * blink.c - a simple program to blink the left LED with about a 1 sec delay
  *
- * left button turns on the left LED
- * right button turns on the right LED
- *
- * Taylor Gamache, 2021-11-09
+ * Taylor Gamache, 2021-11-10
  * */
 #include <msp430fr5994.h>
 
@@ -19,11 +16,12 @@ void main(void)
 
 	// set LED pins as output
 	P1DIR |= BIT0;
-	// light it up
+	// start with it off
 	P1OUT &= ~BIT0;
 
 	while (1) {
 		__delay_cycles(BLINK_CYCLES);
+		// toggle
 		P1OUT ^= BIT0;
 	}
 }
