d5334a8375cba553dbd1900a3040d8a900c4f120
breakthatbass <gamache.taylor@gmail.com>
2021-11-10 11:04:38 -0500

    fix buttons program

--- src/buttons.c
+++ src/buttons.c
@@ -8,8 +8,6 @@
  * */
 #include <msp430fr5994.h>
 
-#define BLINK_CYCLES 5000L
-
 void main(void)
 {
 	// put the dog to sleep
@@ -17,37 +15,40 @@ void main(void)
 	// unlock ports
 	PM5CTL0 &= ~LOCKLPM5;
 
-	// set LED pins as output
+	//led 1
 	P1DIR |= BIT0;
-	P1DIR |= BIT1;
-	// light it up
-	P1OUT |= BIT0;
-	P1OUT |= BIT1;
+	P1OUT &= ~BIT0;
 
-	// button are in port 5 as pins 5 and 6
+	// led 2
+	P1DIR |= BIT1;
+	P1OUT &= ~BIT1;
+
+	// buttons are in port 5 as pins 5 and 6
 	// left button in pin 6, right 5
 	// set bits 5 and 6 as input
-	P5DIR &= ~BIT5;
 	P5DIR &= ~BIT6;
+	P5DIR &= ~BIT5;
 	// enable pull-up/down resistor on pins 5.5 and 5.6
-	P5REN |= BIT5;
 	P5REN |= BIT6;
+	P5REN |= BIT5;
 	// make them pull-ups
-	P5OUT |= BIT5;
 	P5OUT |= BIT6;
+	P5OUT |= BIT5;
 
 	while (1) {
-		__delay_cycles(BLINK_CYCLES);
 		// check if left button is pressed
-		if (P5IN & BIT6) {
+		if ((P5IN & BIT6) == 0) {
 			// light up left LED
 			P1OUT |= BIT0;
-		
-		} else if (P5IN & BIT5) {
+
+		// check if right button is pressed
+		} else if ((P5IN & BIT5) == 0) {
 			// light up right LED
 			P1OUT |= BIT1;
-		} else
-		P1OUT |= 0x0;
-		P1OUT |= 0x0;
+		} else {
+			// turn both LEDS off
+			P1OUT &= ~BIT0;
+			P1OUT &= ~BIT1;
+		}
 	}
 }
