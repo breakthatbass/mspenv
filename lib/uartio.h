3b30ac6cfcd7c12560dc7659110510544df3a6c1
breakthatbass <gamache.taylor@gmail.com>
2021-11-14 16:03:51 -0500

    add io lib

---
+++ lib/uartio.h
@@ -0,0 +1,10 @@
+#ifndef __UARTIO_H__
+#define __UARTIO_H__
+
+void uartprintf(char *format, ...);
+void _puts(char *s);
+void _putc(unsigned b);
+void sendByte(unsigned char byte);
+unsigned char *_gets(unsigned char *s, unsigned int len);
+
+#endif
