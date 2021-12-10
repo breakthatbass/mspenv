8f47b919af74481df201b8b301d044249c6fd740
breakthatbass <gamache.taylor@gmail.com>
2021-12-10 12:23:19 -0500

    redo tiemstamps.c

--- src/temp.c
+++ src/temp.c
@@ -19,25 +19,25 @@ void print_timestamps(void)
         uartprintf("timestamps[%u]: %u\n\r", j, timestamps[j]);
 }
 
-void clock_init(void)
+
+
+int main(void)
 {
+    // put the dawg to sleep
+    WDTCTL = WDTPW | WDTHOLD;
+    //__delay_cycles(1000);
+        // Clock System Setup
+    CSCTL0_H = CSKEY_H;                     // Unlock CS registers
+    CSCTL1 = DCOFSEL_0;                    // set clock to 1mhz
+    CSCTL2 &= ~SELA_7;
+    CSCTL2 |= SELA__VLOCLK;                 // Select ACLK=VLOCLK
+    CSCTL0_H = 0x00;                        // Lock CS module (use byte mode to upper byte)
 
+    // P1.0 (left LED)
 
-    // Timer0_A3 Setup
-    TA0CCTL2 = CM_1 | CCIS_1 | SCS | CAP | CCIE;
-                                            // Capture rising edge,
-                                            // Use CCI2B=ACLK,
-                                            // Synchronous capture,
-                                            // Enable capture mode,
-                                            // Enable capture interrupt
+    //P1OUT &= ~0x01;                         // Clear P1.0 output
+    //P1DIR |= 0x01;                          // Set P1.0 to output direction
 
-    TA0CTL = TASSEL__SMCLK | MC__CONTINUOUS;// Use SMCLK as clock source,
-                                            // Start timer in continuous mode
-}
-
-void temp_init(void)
-{
-    // wait 2 seconds
     __delay_cycles(2000000);
     P5DIR |= BIT2;
     P5OUT &= ~BIT2;
@@ -47,30 +47,11 @@ void temp_init(void)
     // set port as input
     P5DIR &= ~BIT2;
     P5SELC |= BIT2;
-}
-
-int main(void)
-{
-    // put the dawg to sleep
-    WDTCTL = WDTPW | WDTHOLD;
-
-        // Clock System Setup
-    CSCTL0_H = CSKEY_H;                     // Unlock CS registers
-    CSCTL2 &= ~SELA_7;
-    CSCTL2 |= SELA__VLOCLK;                 // Select ACLK=VLOCLK
-    CSCTL0_H = 0x00;                        // Lock CS module (use byte mode to upper byte)
-
-    // P1.0 (left LED)
-    P1OUT &= ~0x01;                         // Clear P1.0 output
-    P1DIR |= 0x01;                          // Set P1.0 to output direction
 
     PM5CTL0 &= ~LOCKLPM5;
+    TA0CCTL2 = CM_1 | CCIS_1 | SCS | CAP | CCIE;
+    TA0CTL = TASSEL__SMCLK | MC__CONTINUOUS;
     uart_init();
-    temp_init();
-    __delay_cycles(1000);                   // Allow clock system to settle
-    clock_init();
-
-
     __bis_SR_register(LPM0_bits | GIE);
     __no_operation();
 }
